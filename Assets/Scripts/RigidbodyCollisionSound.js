4fc3014cce53750652f75a4642290b7daa1e4e4e
Jan-Henrik <jh.bruhn97@gmail.com>
2013-10-20 18:47:28 +0200

    wrote own Array.Contains function because of random unity

--- Assets/Scripts/RigidbodyCollisionSound.js
+++ Assets/Scripts/RigidbodyCollisionSound.js
@@ -6,11 +6,17 @@ var collisionSounds : AudioClip[];
 var possibleColliders : GameObject[];
 
 function OnCollisionEnter(collision : Collision) {
-	if(possibleColliders.Length == 0 || ArrayUtility.Contains(possibleColliders, collision.gameObject)) {
+	if(possibleColliders.Length == 0 || Contains(possibleColliders, collision.gameObject)) {
 		var clip : AudioClip = collisionSounds[Random.Range(0, collisionSounds.length)];
 		audio.PlayOneShot(clip);
 	}
 	
+}
+
+function Contains(array, object) {
+	for(var t in array)
+		if(t == object) return true;
+	return false;
 }
 
 function Start () {

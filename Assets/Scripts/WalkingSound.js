de2e907004dd29474938cd7320cac005327398ed
Jan-Henrik <jh.bruhn97@gmail.com>
2013-10-20 15:33:44 +0200

    Implemented different WalkingSounds for different Terrain textures
    different walkingsounds still missing though

--- Assets/Scripts/WalkingSound.js
+++ Assets/Scripts/WalkingSound.js
@@ -1,13 +1,22 @@
 ﻿#pragma strict
 
-var walkSounds : AudioClip[];
+import System.Collections.Generic;
+
+var walkSoundsGrass : AudioClip[];
+var walkSoundsDirt : AudioClip[];
+var walkSoundsStone : AudioClip[];
+var walkSoundsSand : AudioClip[];
+
 var audioStepLength = .3f;
 
 function Start () {
 	var controller : CharacterController = GetComponent(CharacterController);
     while (true) {
         if (controller.isGrounded && controller.velocity.magnitude > 0.3) {
-            audio.clip = walkSounds[Random.Range(0, walkSounds.length)];
+            
+            var terrainIndex = TerrainSurface.GetMainTexture(controller.transform.position);
+            Debug.Log(terrainIndex);
+            audio.clip = GetRandomSoundWithTerrainIndex(terrainIndex);
 			audio.pitch = Random.Range(0.8f, 1.5f);
             
             audio.Play();
@@ -19,6 +28,22 @@ function Start () {
 
         }
     }
+}
+
+function GetRandomSoundWithTerrainIndex(terrainIndex : int) {
+	var selectedArray : AudioClip[] = walkSoundsGrass;
+	
+	switch(terrainIndex) {
+		case 0: selectedArray = walkSoundsGrass; break;
+		case 1: selectedArray = walkSoundsDirt; break;
+		case 2: selectedArray = walkSoundsStone; break;
+		case 3: selectedArray = walkSoundsSand; break;
+	}
+	
+	if(selectedArray.Length > 0)
+		return walkSoundsGrass[Random.Range(0, walkSoundsGrass[terrainIndex].length)];
+	
+	return null;
 }
 
 function Update () {

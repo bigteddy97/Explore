24ed5105c08011c554f9b0d25551126110716cba
Jan-Henrik <jh.bruhn97@gmail.com>
2013-10-20 18:43:34 +0200

    more sounds, less random shit, maybe AA, ART

---
+++ Assets/Scripts/RigidbodyCollisionSound.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+@script RequireComponent(AudioSource)
+
+var collisionSounds : AudioClip[];
+var possibleColliders : GameObject[];
+
+function OnCollisionEnter(collision : Collision) {
+	if(possibleColliders.Length == 0 || ArrayUtility.Contains(possibleColliders, collision.gameObject)) {
+		var clip : AudioClip = collisionSounds[Random.Range(0, collisionSounds.length)];
+		audio.PlayOneShot(clip);
+	}
+	
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}

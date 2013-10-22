bd274e7063869250eb6ffa9b8d1f437b2bb4d2f3
Jan-Henrik <jh.bruhn97@gmail.com>
2013-10-22 15:45:05 +0200

    käsewurst

---
+++ Assets/Scripts/TextureShifter.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var materialIndex : int = 0;
+var uvAnimationRate : Vector2 = new Vector2(1.0f, 1.0f);
+var textureName : String = "_MainTex";
+
+private var uvOffset : Vector2 = Vector2.zero;
+
+function Update () {
+	uvOffset += (uvAnimationRate * Time.deltaTime);
+	if(renderer.enabled) {
+		renderer.materials[materialIndex].SetTextureOffset(textureName, uvOffset);
+	}
+}

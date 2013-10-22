be785f87f2c5257aa074a6a5ec3b07451981d42c
Jan-Henrik <jh.bruhn97@gmail.com>
2013-10-22 17:46:39 +0200

    added better trees to terrain and grass and stones <3

---
+++ Assets/Editor/RandomizeRotationAndScale.js
@@ -0,0 +1,16 @@
+﻿public class RandomizeRotationAndScale extends EditorWindow {	
+	@MenuItem ("Window/Randomize Rotation and Scale, Bitch")
+	static function ShowWindow () {
+		var window = EditorWindow.GetWindow(RandomizeRotationAndScale);
+	}
+	
+	function OnGUI () {
+		if(GUILayout.Button("DoStuff!")) {
+			for(var o : GameObject in Selection.objects) {
+				o.transform.eulerAngles.y = Random.Range(0, 360);
+				var randomValue = (Random.value - .5f);
+				o.transform.localScale += new Vector3(randomValue, randomValue, randomValue);
+			}
+		}
+	}
+}

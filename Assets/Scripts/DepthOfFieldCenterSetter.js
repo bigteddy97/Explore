2e5c6040cf5af85018c9836a79533a33d7f6419d
Jan-Henrik <jh.bruhn97@gmail.com>
2013-11-06 17:40:16 +0100

    added some sweet shaders

---
+++ Assets/Scripts/DepthOfFieldCenterSetter.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var dofCenter : Transform;
+
+function Update () {
+	var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+    var hit : RaycastHit;
+    if (Physics.Raycast (ray, hit, 1000)) {
+    	dofCenter.position = (hit.point);
+    }
+}
